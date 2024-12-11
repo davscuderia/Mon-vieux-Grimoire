6ca0e45027d9a24fe73531305aff148b0f63614b
Dav Prat <scuderia1980@gmail.com>
2024-12-11 20:08:49 +0100

    config de mongodb, ajout des models book et user et test ok, de communication postman

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require('express')
-
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://Dav-Admin:Eyqki1wyMl5xzzLm@cluster0.dikgl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const Book = require('./models/Book')
+const User = require('./models/User')
+
+app.use(express.json())
 
 app.use((req, res, next) => {
-    console.log('requete reçue !')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
     next()
 })
 
-app.use((req, res, next) => {
-    res.status(201)
-    next()
-})
-
-app.use((req, res, next) => {
-    res.json({ message: 'test réponse reçue !'})
+app.get('/api/books', (req, res, next) => {
+    res.status(200).json({
+    message: 'livre créé !'
+    })
 })
 
 module.exports = app
