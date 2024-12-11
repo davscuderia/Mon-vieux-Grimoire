8594c3c6f9106d65c6db29d70ece1328808f944f
Dav Prat <scuderia1980@gmail.com>
2024-12-11 14:58:38 +0100

    ajout des fonctions normalizePort et errorHandler au serveur

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res) => {
+app.use((req, res, next) => {
+    console.log('requete reçue !')
+    next()
+})
+
+app.use((req, res, next) => {
+    res.status(201)
+    next()
+})
+
+app.use((req, res, next) => {
     res.json({ message: 'test réponse reçue !'})
 })
 
