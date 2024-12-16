d15c0dd1cc94ac7151186563022d420d56e8a3fe
Dav Prat <scuderia1980@gmail.com>
2024-12-16 19:06:33 +0100

    ajout nouvel utilisateur et connexion utilisateur

--- routes/Stuff.js
+++ routes/Stuff.js
@@ -1,17 +1,18 @@
 const express = require('express')
+const auth = require('../middleware/auth')
+const multer = require('../middleware/multer-config')
 const router = express.Router()
 
 const stuffCtrl = require('../controllers/Stuff')
 const User = require('../models/User')
 
-router.get('/', stuffCtrl.getAllStuff)
-
-router.get('/bestrating', stuffCtrl.bestRating)
-router.post('/', stuffCtrl.createBook)
-router.post('/:id/rating', stuffCtrl.rateBook)
-router.get('/:id', stuffCtrl.getOneBook)
-router.put('/:id', stuffCtrl.modifyBook)
-router.delete('/:id', stuffCtrl.deleteBook)
+router.get('/', auth, stuffCtrl.getAllStuff)
+router.get('/bestrating', auth, stuffCtrl.bestRating)
+router.post('/', auth, stuffCtrl.createBook)
+router.post('/:id/rating', auth, stuffCtrl.rateBook)
+router.get('/:id', auth, stuffCtrl.getOneBook)
+router.put('/:id', auth,  stuffCtrl.modifyBook)
+router.delete('/:id', auth, stuffCtrl.deleteBook)
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
